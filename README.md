b601e7143924061a24bcd172df587f01a26b5a7b
kyeshmz <kyeshimizu@gmail.com>
2024-01-30 09:24:18 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Types for Touchdesigner Signaling API
+
+This is a types package for Touchdesigner Signaling API.
+This is created from the Schemas defined by Derivative [here](https://github.com/TouchDesigner/SignalingAPI/tree/main).
+
+## License
+
+MIT
---
+++ index.d.ts
@@ -0,0 +1,82 @@
+export type Answer = {
+	metadata: Metadata;
+	signalingType: "Answer";
+	sender?: string;
+	target: string;
+	content: {
+		sdp: string;
+	};
+};
+
+export type Client = {
+	id: string;
+	address: string;
+	properties?: Object;
+};
+
+export type ClientEnter = {
+	metadata: Metadata;
+	signalingType: "ClientEnter";
+	content: {
+		client: Client;
+	};
+};
+
+export type ClientEntered = {
+	metadata: Metadata;
+	signalingType: "ClientEntered";
+	content: {
+		self: Client;
+	};
+};
+
+export type ClientExit = {
+	metadata: Metadata;
+	signalingType: "ClientExit";
+	content: {
+		client: Client;
+	};
+};
+
+export type Clients = {
+	metadata: Metadata;
+	signalingType: "Clients";
+	content: {
+		clients: Client[];
+	};
+};
+
+export type Ice = {
+	metadata: Metadata;
+	signalingType: "Ice";
+	sender?: string;
+	target: string;
+	content: {
+		sdpCandidate: string;
+	};
+};
+
+export type Message = {
+	metadata: Metadata;
+	signalingType: string;
+	sender?: string;
+	target?: string;
+	content?: Object;
+};
+
+export type Metadata = {
+	description: string;
+	compVersion: string;
+	compOrigin: string;
+	projectName: string;
+};
+
+export type Offer = {
+	metadata: Metadata;
+	signalingType: "Offer";
+	sender?: string;
+	target: string;
+	content: {
+		sdp: string;
+	};
+};
